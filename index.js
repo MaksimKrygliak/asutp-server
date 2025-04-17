3b13b8b826bf274a3b2b71572b1a617d009e9132
MAX <maksimkrygliak@gmail.com>
2025-04-18 01:54:06 +0300

    18.04.25

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-import express from 'express';
-import fs from 'fs';
-import multer from 'multer';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
-import { handleValidationErrors, checkAuth } from './utils/index.js';
-import { UserController, PostController } from './controllers/index.js';
-import { verifyAdminRole } from './utils/verifyRole.js';
+import express from "express";
+import fs from "fs";
+import multer from "multer";
+import cors from "cors";
+import mongoose from "mongoose";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
+import { handleValidationErrors, checkAuth } from "./utils/index.js";
+import { UserController, PostController } from "./controllers/index.js";
+import { verifyAdminRole } from "./utils/verifyRole.js";
 
-// const mongoUri = process.env.MONGODB_URI;
-// if (!mongoUri) {
-//   console.error('Ошибка: Переменная окружения MONGODB_URI не определена.');
-//   process.exit(1); // Завершить процесс с ошибкой
-//   // Или выбросить ошибку: throw new Error('MONGODB_URI is not defined');
-// }
+const mongoUri = process.env.MONGODB_URI;
+if (!mongoUri) {
+  console.error("Ошибка: Переменная окружения MONGODB_URI не определена.");
+  process.exit(1);
+}
 
 mongoose
-  .connect('mongodb+srv://maksimkryglyk:prometey888@asutp.ofqp3js.mongodb.net/asutp')
-  // .connect(mongoUri)
-  .then(() => console.log('DB ok'))
-  .catch((err) => console.log('DB error', err));
+  // .connect('mongodb+srv://maksimkryglyk:prometey888@asutp.ofqp3js.mongodb.net/asutp')
+  .connect(mongoUri)
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
 
 const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
-    if (!fs.existsSync('uploads')) {
-      fs.mkdirSync('uploads');
+    if (!fs.existsSync("uploads")) {
+      fs.mkdirSync("uploads");
     }
-    cb(null, 'uploads');
+    cb(null, "uploads");
   },
   filename: (_, file, cb) => {
     cb(null, file.originalname);
@@ -39,37 +42,53 @@ const upload = multer({ storage });
 
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
-app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
-app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
-app.get('/auth/verify/:token', UserController.verifyEmail);
-app.get('/auth/me', checkAuth, UserController.getMe);
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+app.get("/auth/verify/:token", UserController.verifyEmail);
+app.get("/auth/me", checkAuth, UserController.getMe);
 
-app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
   res.json({
     url: `/uploads/${req.file.originalname}`,
   });
 });
 
-app.get('/tags', PostController.getLastTags);
-app.get('/posts', PostController.getAll);
-app.get('/posts/tags', PostController.getLastTags);
-app.get('/posts/:id', PostController.getOne);
-app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
-app.delete('/posts/:id', checkAuth, verifyAdminRole, PostController.remove);
+app.get("/tags", PostController.getLastTags);
+app.get("/posts", PostController.getAll);
+app.get("/posts/tags", PostController.getLastTags);
+app.get("/posts/:id", PostController.getOne);
+app.post(
+  "/posts",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.create
+);
+app.delete("/posts/:id", checkAuth, verifyAdminRole, PostController.remove);
 app.patch(
-  '/posts/:id',
+  "/posts/:id",
   checkAuth,
   verifyAdminRole,
   postCreateValidation,
   handleValidationErrors,
-  PostController.update,
+  PostController.update
 );
 
 app.listen(process.env.PORT || 4000, (err) => {
   if (err) {
     return console.log(err);
   }
-  console.log('Server OK');
+  console.log("Server OK");
 });
